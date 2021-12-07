07e949fd24d8581fc00b6564d3a6ae37ca2d3959
ohtjqkd <ohtjqkd@gmail.com>
2021-12-07 21:41:50 +0900

    init commit

--- portfolio/apps.py
+++ portfolio/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+import json, os
 
 class PortfolioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'portfolio'
+    static_datasource = json.loads(open('./portfolio/datasource.json').read())
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from .apps import PortfolioConfig
 # Create your views here.
 
 class AppHome(APIView):
     def get(self, request):
         print('hello')
-        return render(request, 'portfolio/index.html', context={})
+        print(PortfolioConfig.static_datasource)
+        print(type(PortfolioConfig.static_datasource))
+        return render(request, 'portfolio/index.html', context=PortfolioConfig.static_datasource)
